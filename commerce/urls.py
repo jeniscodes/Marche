290aec2cadf9e34c47bdb9344fefdb9d6c8f6633
jeniscodes <61928262+jeniscodes@users.noreply.github.com>
2020-07-12 22:14:35 +0545

    all

--- commerce/urls.py
+++ commerce/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls")),
     path("login", auth_views.LoginView.as_view())
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
